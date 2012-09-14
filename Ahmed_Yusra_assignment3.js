6bb22778db4e775fa8b8a54437b8a352e54bb294
Yusra Ahmed <Yusra718@fullsail.edu>
2012-09-13 23:39:42 -0400

    Procedure finished

--- Ahmed_Yusra_assignment3.js
+++ Ahmed_Yusra_assignment3.js
@@ -2,4 +2,26 @@
 // SDI 1209
 // Project 3
 
+//Declare Variables
+var prayers = ["Dawn", "Midday", "Afternoon", "Dusk", "Evening"];
+
+//Procedure
+
+var procedure = function(eat, prayed) {
+    console.log("I wake up at 3 am.")
+    if (eat === "ate"){
+        console.log("I ate already.")
+        if (prayed === "already prayed"){
+            console.log("I prayed already. I can go back to sleep now.")
+        }else {
+            console.log("I have to pray " + prayers[0] + " prayer before I can go back to sleep.")
+        }
+    }else{
+        console.log("I have to eat in order to be ready for my big day.")
+        console.log("Then I have to pray " + prayers[0] + " prayer before going back to sleep.")
+    };
+};
+
+//Main Code
+procedure("ate", "already prayed")
 console.log("I will eat " + samosa.amountForMe(15, 8) + " samosas." )
