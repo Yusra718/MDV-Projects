d0d2645d331d93e336ff92c86904f8ff02e1af41
Yusra Ahmed <Yusra718@fullsail.edu>
2012-09-14 22:19:36 -0400

    Much more work done

--- Ahmed_Yusra_assignment3.js
+++ Ahmed_Yusra_assignment3.js
@@ -19,7 +19,7 @@ var procedure = function(eat, prayed) {
     }else{
         console.log("I have to eat in order to be ready for my big day.")
         console.log("Then I have to pray " + prayers[0] + " prayer before going back to sleep.")
-    };
+    }
 };
 
 // Boolean Function
@@ -35,9 +35,13 @@ var booleanFunction = function(reciteQuran, assignments) {
         };
     }else{
         console.log("Let me recite some Quran before logging on to Fullsail.");
-    };
-    if (reciteQuran === true, assignments === true){
-        return true
+    }
+    if (reciteQuran === true){
+        if (assignments === true){
+            return true
+        }else{
+            return false
+        }
     }else{
         return false
     }
@@ -47,50 +51,76 @@ var booleanFunction = function(reciteQuran, assignments) {
 
 var quote = function(first, next){
 
-    var say = first + samosa.type + next;
-    return say
+    var string = first + samosa.type + next;
+    return string
 };
 
 // Return Array
 
-var pray = function(something) {
-    var j = something.length
+var pray = function(array) {
+    var j = array.length
     ;
     for(var i=0; i<j; i++) {
-        console.log("I have to pray " + something[i] + " prayer, before I continue doing anything else.")
+        console.log("I have to pray " + array[i] + " prayer, before I continue doing anything else.")
         
         var     k = i + 1;
         
         if (i<4) {
             console.log("I've only prayed " + k + " prayers so far, I'm not done with today's prayers.")
         }else{
-            console.log("The last prayer was the " + something[4] + " prayer, and I'm happy that I've done what God has asked me to do.")
+            console.log("The last prayer was the " + array[4] + " prayer, and I'm happy that I've done what God has asked me to do.")
         };
     };
-    return something
+    return array
 };
 
-// Array Function
+// Object Function
+
+var samosaRecipe = function(object) {
+    for (var key in object) {
+        console.log(key + ": " + object[key])
+    }
+    return object
+};
+
+// Loops
 
 var makeSamosa = function(array1, array2) {
-
-    for (var key in samosa) {
-        console.log(key + ": " + samosa[key])
-    }
-    console.log("The ingredients needed to make samosas are:")
-    for (var key in array1){
-        console.log("*" + array1[key])
-    }
-    console.log("The sesonings used to make samosas taste delicious are:")
-    for (var key in array2) {
-        console.log("*" + array2[key])
-    }
+                        console.log("The ingredients needed to make samosas are:")
+                        for (var key in array1){
+                            console.log("*" + array1[key])
+                        }
+                        console.log("The sesonings used to make samosas taste delicious are:")
+                        for (var key in array2) {
+                            console.log("*" + array2[key])
+                        }
 };
 
+//Number Function
+var eatSamosas = function(){
+        var h = 15,
+        l = 8,
+        mine = h-l
+        ;
+    console.log("There are " + h + " samosas, I'll eat one.")
+    while (h > l) {
+        h--
+        if (h>l) {
+            console.log("There are " + h + " left. I'l stuff my face with one more.")
+        }else{
+            console.log("I ate " + mine + " samosas. I'll leave " + l + " for everyone else.")
+        }
+    }
+}
+
 //Main Code
+
 procedure("ate", "already prayed")
-booleanFunction(true, true)
+console.log("The fact that I have recited Quran and finished my assignments is: " + booleanFunction(true, true))
 console.log(quote("I'm going to make samosas with ", ". These taste delicious."))
+console.log(samosaRecipe(samosa.recipe))
+samosa.propertiesProcedure(samosa)
 makeSamosa(samosa.recipe.Ingredients, samosa.recipe.Seasoning)
 console.log("I will eat " + samosa.amountForMe(15, 8) + " samosas." )
+eatSamosas()
 console.log(pray(prayers))
--- Json.js
+++ Json.js
@@ -1,14 +1,20 @@
 var samosa ={
     
-    "type":             "Chunks of Potatoes",
-    "howMany":          15,
-    "recipe":           {
-                            "Ingredients": ["Onions", "Peppers", "Ground Beef", "Scallions", "Potato Chunks"],
-                            "Seasoning":        ["Curry Powder", "Cumin Powder", "Goya Seasoning", "Paprika"]
-                        },
-    "isDelicious":      true,
-    "amountForMe":      function(howMany, amountForRest){
-        var amountIEat = howMany - amountForRest;
-        return amountIEat
-    }
+    "type":                     "Chunks of Potatoes",
+    "howMany":                  15,
+    "recipe":                   {
+                                    "Ingredients": ["Onions", "Peppers", "Ground Beef", "Scallions", "Potato Chunks"],
+                                    "Seasoning":        ["Curry Powder", "Cumin Powder", "Goya Seasoning", "Paprika"]
+                                },
+    "isDelicious":              true,
+    "amountForMe":              function(howMany, amountForRest){
+                                    var amountIEat = howMany - amountForRest;
+                                    return amountIEat
+                                },
+    "propertiesProcedure":      function(mainObject) {
+                                    console.log("Here are the properties of the delicious samosas: ")
+                                    for (var key in mainObject) {
+                                        console.log(key + ": " + mainObject[key])
+                                    }
+                                }
 };
